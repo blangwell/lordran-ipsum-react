46f261ef62b04758872e8602b7107cbaf26eff18
blangwell <b.langwell@outlook.com>
2021-04-03 13:45:42 -0700

    Add bonfire gif and font files, update styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // import { useState } from 'react';
 import './App.css';
 import TextBox from './Textbox';
+import bonfire from './bonfire-pixel.gif';
+import './fonts/Dico.ttf';
+import './fonts/Dico2.ttf';
 
 function App() {  
   return (
@@ -8,6 +11,7 @@ function App() {
       <h1 className="title">Lordran Ipsum</h1>
       <p className="subtitle">Dark Souls NPC Dialogue Lorem Ipsum</p>
       <TextBox length="sentence" />
+      <img src={bonfire} alt="pixelated bonfire" id="pixel-bonfire"/>
     </div>
   );
 }
